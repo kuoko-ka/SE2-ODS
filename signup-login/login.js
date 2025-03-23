25d996e8a59197521a6844702c9ce3b8ade45eae
Jay-350 <jayrosellosa@gmail.com>
2025-03-23 17:22:50 +0800

    Add files via upload
    login and signup

---
+++ signup-login/login.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXVPjlFDkINP22f4aSPPJa8IglQTg-GsA",
+  authDomain: "odsdb-48682.firebaseapp.com",
+  projectId: "odsdb-48682",
+  storageBucket: "odsdb-48682.firebasestorage.app",
+  messagingSenderId: "916898800921",
+  appId: "1:916898800921:web:9c80282c2efc68b91c5188"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//submit button
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function(event){
+event.preventDefault()
+
+//inputs 
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("logging in")
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("Error")
+    // ..
+  });
+})
---
+++ signup-login/readme.txt
@@ -0,0 +1,2 @@
+need mag install ng live server extension sa vs studio then while naka click sa html file 
+pindutin yung go line sa bottom left and that should do it.
