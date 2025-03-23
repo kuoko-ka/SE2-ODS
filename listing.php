ede7d450de4295c9a2c095541718926de4ac00e9
Trabby-Patty <12agoncillo.traballojhonwazel@gmail.com>
2025-03-23 19:17:29 +0800

    view list daw pang client

---
+++ listing.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>On-Demand Services</title>
+    <link rel="icon" href="ods_img/odsicon.png" type="image/png">
+    <link rel="stylesheet" href="listing.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="ods_img/miniodsicon.png" alt="ODS WebApp">
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <button class="profile_btn">
+            </button>
+        </nav>
+    </header>
+
+    <div class="container">
+        <a class="create" href="create-list">Create Task</a>
+        <div class="inner-cont">
+            <div class="ratings">
+                <h2>Ratings You Made</h2>
+                <ul class="text">
+                    <li>Rating: 4.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.2 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 2.8 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.9 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.2 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 2.8 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.9 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.2 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 2.8 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 3.5 <p class="stars">★★★★★</p></li>
+                    <li>Rating: 4.9 <p class="stars">★★★★★</p></li>
+                </ul>   
+            </div>
+            <div class="view">
+                <div class="task">
+                    <p>You currently have no listing.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+    <p>&copy; <?php echo date("Y"); ?> ODS WebApp. All Rights Reserved.</p>
+    </footer>
+</body>
+</html>
